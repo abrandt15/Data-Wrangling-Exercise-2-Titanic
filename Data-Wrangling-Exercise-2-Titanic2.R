5192a38972f1e011eff052f538719dab5e78430b
Alex Brandt <alexbrandt15@gmail.com>
2018-10-16 20:38:05 -0400

    Update to Titanic exercise.

--- Data-Wrangling-Exercise-2-Titanic2.R
+++ Data-Wrangling-Exercise-2-Titanic2.R
@@ -3,12 +3,12 @@ titanic <- read_csv("titanic_original.csv")
 head(titanic)
 str(titanic)
 titanic$embarked
-View (titanic)
+View(titanic)
 
 #Port of embarkation - Replace Blank cells with "S"
 
-titanic %>% mutate(embarked = gsub("^$|^ $", "S", embarked))
-titanic %>% mutate(embarked = gsub("NA", "S", embarked))
+titanic %>% mutate(embarked = gsub("^$|^ $", "S", embarked) %>%
+  gsub("NA", "S", .))
 
 
 #Age - Populate missing values with mean of age column.
@@ -19,17 +19,26 @@ mean_age <- mean(titanic$age, na.rm = TRUE)
 print(mean_age)
 
 #Replace NA values with mean age
-mean_age <- titanic$age[is.na(titanic$age)]
-print (mean_age)
+titanic$age[is.na(titanic$age)]<- mean_age
+print(mean_age)
+is.na(titanic$age) %>% sum()
+titanic$age[1:15]
+titanic$age[c(TRUE, FALSE)]
 
 #Lifeboat
 
-titanic$boat <- gsub("^$", "NA", titanic$boat)
-
+titanic$boat <- gsub("", NA , titanic$boat)
+table(titanic$boat)
+isTRUE(TRUE)
 
 
 #Cabin
 
-titanic$has_cabin_number <- titanic %>% mutate(has_cabin_number = ifelse(titanic$cabin != 'Null',1,0))
+titanic <- titanic %>% mutate(has_cabin_number = ifelse(is.na(cabin),1,0))
+head(titanic)
+titanic$has_cabin_number
 
-
+is.null(titanic$cabin) %>% sum()
+is.na(titanic$cabin) %>% sum()
+nrow(titanic)
+table(titanic$cabin)      
